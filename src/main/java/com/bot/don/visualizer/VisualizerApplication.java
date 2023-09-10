f4b130a47953619c53ab7a8f68138a5e2de6dbe1
Pablo Vazquez <pablov@galdevs.com>
2023-09-10 06:37:19 +0200

    empty project, from inizilitzer
    empty project, from inizilitzer

---
+++ src/main/java/com/bot/don/visualizer/VisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.bot.don.visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bot/don/visualizer/VisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.don.visualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
